5e3dba4f6edbc90ecc76d5128b46a555c0a065f9
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2023-12-03 18:28:47 +0530

    initial

---
+++ android/app/src/main/java/com/example/flutter_map_booking/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.flutter_map_booking;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {}
---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+class Config{
+  static const String apiKey = 'api_key';
+  static const String language = 'vi';
+  static const String region = 'VN';
+}
